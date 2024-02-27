1ed1633cf9eb728eac2c745fa67d871061fd7324
Jan Erik Breimo <jebreimo@gmail.com>
2024-02-27 22:35:31 +0100

    Fix GCC build.

--- include/Chorasmia/IntervalMap.hpp
+++ include/Chorasmia/IntervalMap.hpp
@@ -6,6 +6,7 @@
 // License text is included with the source distribution.
 //****************************************************************************
 #pragma once
+#include <limits>
 #include <vector>
 
 namespace Chorasmia
