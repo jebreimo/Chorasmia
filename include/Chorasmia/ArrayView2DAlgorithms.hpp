4593920838cc62ad16b552218d146abdb9324ec1
Jan Erik Breimo <jebreimo@gmail.com>
2024-03-03 12:19:10 +0100

    Add interpolate_value for array views.

--- include/Chorasmia/ArrayView2DAlgorithms.hpp
+++ include/Chorasmia/ArrayView2DAlgorithms.hpp
@@ -7,6 +7,7 @@
 //****************************************************************************
 #pragma once
 #include <algorithm>
+#include <cmath>
 #include "MutableArrayView2D.hpp"
 #include "Index2DMap.hpp"
 
@@ -52,5 +53,90 @@ namespace Chorasmia
                 dst(i, j) = src(mapping.get_from_indices(i, j));
             }
         }
+    }
+
+    /**
+     * @brief A concept for types that can be multiplied by a scalar.
+     */
+    template <typename T>
+    concept ScalarMultipliable = requires(T a, double b)
+    {
+        { a * b } -> std::convertible_to<T>;
+    };
+
+    /**
+     * @brief A concept for types that can be added to each other.
+     */
+    template <typename T>
+    concept Addable = requires(T a, T b)
+    {
+        { a + b } -> std::convertible_to<T>;
+    };
+
+    // A concept for types that are Addable and ScalarMultipliable.
+    template <typename T>
+    concept AddableAndScalarMultipliable = Addable<T> && ScalarMultipliable<T>;
+
+    /**
+     * @brief Interpolates a value in a 2D array using bilinear interpolation.
+     * @param array The array to interpolate in.
+     * @param i The row index of the point to interpolate.
+     * @param j The column index of the point to interpolate.
+     * @return The interpolated value.
+     */
+    template <AddableAndScalarMultipliable T>
+    T interpolate_value(const ArrayView2D<T>& array, double i, double j)
+    {
+        bool on_boundary = false;
+
+        double i_int;
+        auto i_frac = std::modf(i, &i_int);
+        const auto i_idx = size_t(i_int);
+        if (i_int < 0 || i_frac < 0 || array.row_count() - 1 <= i_idx)
+        {
+            if (array.row_count() - 1 != i_idx || i_frac != 0)
+                CHORASMIA_THROW("i lies outside the array.");
+            on_boundary = true;
+        }
+
+        double j_int;
+        auto j_frac = std::modf(j, &j_int);
+        const auto j_idx = size_t(j_int);
+        if (j_int < 0 || j_frac < 0 || array.col_count() - 1 <= j_idx)
+        {
+            if (array.col_count() - 1 != j_idx || j_frac != 0)
+                CHORASMIA_THROW("j lies outside the array.");
+            on_boundary = true;
+        }
+
+        double weights[4] =
+            {
+                (1 - i_frac) * (1 - j_frac),
+                i_frac * (1 - j_frac),
+                (1 - i_frac) * j_frac,
+                i_frac * j_frac
+            };
+
+        if (!on_boundary)
+        {
+            // If we're not on the boundary we can use all the array values.
+            return array(i_idx, j_idx) * weights[0]
+                   + array(i_idx + 1, j_idx) * weights[1]
+                   + array(i_idx, j_idx + 1) * weights[2]
+                   + array(i_idx + 1, j_idx + 1) * weights[3];
+        }
+
+        // If we're on the boundary we only get the array values that are
+        // inside the array.
+        T result = {};
+        if (weights[0] != 0)
+            result += array(i_idx, j_idx) * weights[0];
+        if (weights[1] != 0)
+            result += array(i_idx + 1, j_idx) * weights[1];
+        if (weights[2] != 0)
+            result += array(i_idx, j_idx + 1) * weights[2];
+        if (weights[3] != 0)
+            result += array(i_idx + 1, j_idx + 1) * weights[3];
+        return result;
     }
 }
--- tests/ChorasmiaTest/test_ArrayView2DAlgorithms.cpp
+++ tests/ChorasmiaTest/test_ArrayView2DAlgorithms.cpp
@@ -8,6 +8,11 @@
 #include <Chorasmia/ArrayView2DAlgorithms.hpp>
 #include <Chorasmia/Array2D.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <Xyz/Vector.hpp>
+
+using Catch::Matchers::WithinAbs;
+constexpr auto MARGIN = Xyz::Constants<double>::DEFAULT_MARGIN;
 
 TEST_CASE("Test copy")
 {
@@ -21,4 +26,74 @@ TEST_CASE("Test copy")
     REQUIRE(b(1, 1) == 2);
     REQUIRE(b(2, 0) == 4);
     REQUIRE(b(2, 1) == 1);
+}
+
+TEST_CASE("Test that interpolate_value supports non-primitive types")
+{
+    using namespace Chorasmia;
+    Array2D<Xyz::Vector2D> a(
+        {
+            {0, 1}, {1, 2}, {2, 3},
+            {4, 5}, {5, 6}, {6, 7},
+            {8, 9}, {9, 10}, {10, 11}
+        }, 3, 3);
+    REQUIRE(interpolate_value(a.view(), 0.5, 0.5) == Xyz::Vector2D(2.5, 3.5));
+    REQUIRE(interpolate_value(a.view(), 0.5, 0) == Xyz::Vector2D(2, 3));
+    REQUIRE(interpolate_value(a.view(), 2, 1.5) == Xyz::Vector2D(9.5, 10.5));
+    REQUIRE(interpolate_value(a.view(), 2, 2) == Xyz::Vector2D(10, 11));
+}
+
+TEST_CASE("Test that interpolate_value behaves correctly on the boundary")
+{
+    using namespace Chorasmia;
+    Array2D<double> a(
+        {
+            0, 1, 2,
+            1, 2, 3,
+            2, 3, 4
+        }, 3, 3);
+    REQUIRE(interpolate_value(a.view(), 0, 0) == 0);
+    REQUIRE(interpolate_value(a.view(), 0, 0.5) == 0.5);
+    REQUIRE(interpolate_value(a.view(), 0, 1) == 1);
+    REQUIRE(interpolate_value(a.view(), 0, 1.5) == 1.5);
+    REQUIRE(interpolate_value(a.view(), 0, 2) == 2);
+
+    REQUIRE(interpolate_value(a.view(), 0.5, 0) == 0.5);
+    REQUIRE(interpolate_value(a.view(), 0.5, 2) == 2.5);
+
+    REQUIRE(interpolate_value(a.view(), 1, 0) == 1);
+    REQUIRE(interpolate_value(a.view(), 1, 2) == 3);
+
+    REQUIRE(interpolate_value(a.view(), 1.5, 0) == 1.5);
+    REQUIRE(interpolate_value(a.view(), 1.5, 2) == 3.5);
+
+    REQUIRE(interpolate_value(a.view(), 2, 0) == 2);
+    REQUIRE(interpolate_value(a.view(), 2, 0.5) == 2.5);
+    REQUIRE(interpolate_value(a.view(), 2, 1) == 3);
+    REQUIRE(interpolate_value(a.view(), 2, 1.5) == 3.5);
+    REQUIRE(interpolate_value(a.view(), 2, 2) == 4);
+}
+
+TEST_CASE("Test that interpolate_value throws when x or y is outside the array")
+{
+    using namespace Chorasmia;
+    Array2D<double> a({1, 2, 3, 4, 5, 6}, 2, 3);
+    REQUIRE_THROWS_AS(interpolate_value(a.view(), -0.1, 0), ChorasmiaException);
+    REQUIRE_THROWS_AS(interpolate_value(a.view(), -2, 0), ChorasmiaException);
+    REQUIRE_THROWS_AS(interpolate_value(a.view(), 1.1, 0), ChorasmiaException);
+    REQUIRE_THROWS_AS(interpolate_value(a.view(), 0, -0.1), ChorasmiaException);
+    REQUIRE_THROWS_AS(interpolate_value(a.view(), 0, -100), ChorasmiaException);
+    REQUIRE_THROWS_AS(interpolate_value(a.view(), 0, 2.3), ChorasmiaException);
+}
+
+TEST_CASE("Test that interpolate_value interpolates correctly")
+{
+    using namespace Chorasmia;
+    Array2D<double> a(
+        {
+            0, 1, 2,
+            1, 2, 3,
+            2, 3, 4
+        }, 3, 3);
+    REQUIRE_THAT(interpolate_value(a.view(), 0.2, 1.3), WithinAbs(1.5, MARGIN));
 }
